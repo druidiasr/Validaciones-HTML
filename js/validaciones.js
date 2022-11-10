988b549790d3313dddc41698dcfcfcc32f0e9d9c
DavidRR <druidiasr@gmail.com>
2022-11-10 17:18:42 -0500

    aula4 completada

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,13 +1,62 @@
 export function valida(input) {
-    const tipoDeImput = input.dataSet.tipo;
+    const tipoDeImput = input.dataset.tipo;
     if (validadores[tipoDeImput]) {
     validadores[tipoDeImput](input);
     }
+    
+    if(input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeImput, input);
+    }
 }
 
-const validadores = {
-    nacimiento: input => validarNacimiento(input)
+const tipoDeErrorres = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "El campo nombre no puede estar vacio",
+    },
+    email: {
+        valueMissing: "El campo correo no puede estar vacio",
+        typeMismatch: "El correo no es valido",
+    },
+    password: {
+        valueMissing: "El campo contraseña no puede estar vacio",
+        patternMismatch: "Mínimo ocho y máximo 10 caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial",
+    },
+    nacimiento: {
+        valueMissing: "El campo fecha de nacimiento no puede estar vacio",
+        customError: "Debes tener al menos 18 años de edad", 
+    },
+    
 };
+
+const validadores = {
+    nacimiento: (input) => validarNacimiento(input),
+};
+
+function mostrarMensajeDeError(tipoDeImput, input) {
+    let mensaje = "";
+    tipoDeErrorres.forEach((error) => {
+        if(input.validity[error]) {
+            console.log(tipoDeImput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeImput][error]);
+            mensaje = mensajesDeError[tipoDeImput][error];
+        }
+    });
+    return(mensaje);
+}
 
 function validarNacimiento(input) {
     const fechaCliente = new Date(input.value);
